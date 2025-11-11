12f987631e0c614801f7e8c996d265b5aea4871e
George Kirby <georgekirby2003@gmail.com>
2025-11-11 11:47:15 +0000

    mt

--- Matlab/Learning/Solvers/dynamics.m
+++ Matlab/Learning/Solvers/dynamics.m
@@ -1,8 +1,20 @@
 function dx = dynamics(t, y, solution, current_fun)
+
 R1 = solution(end);
 C = solution(end-2);
 Q = solution(end-3);
 current = current_fun(t);
+
+if y(1) <=0.01 && current< 0
+    y(1) =0;
+    current =0;
+end
+
+if y(1) >=1 && current> 0
+    y(1) =1;
+    current =0;
+end
+
 dx = zeros(2,1);
 dx(1) = current./Q;
 dx(2) = -y(2)./(R1.*C) + current./C;
--- Matlab/Learning/Solvers/solving.m
+++ Matlab/Learning/Solvers/solving.m
@@ -4,7 +4,7 @@ load('..\Fixed_Poly\DS_DATA.mat')
 load('..\LiPO_Parameter_Estimation\Data\cycle_1.mat')
 
 current_lut = @(t) interp1(tt, u1, t, 'linear', 'extrap');
-
+% p(12)= 2*p(12); % the problem from before was the soc went negative, and then the ocv went went. it's not used to fitting for negative soc. 
 [t, y] = ode45(@(t, y) dynamics(t, y, p, current_lut), [0 tt(end)], [1; -0.10]);
 
 x1=y(:,1);x2=y(:,2); R0 = p(end-1);
@@ -18,4 +18,3 @@ figure
 
 plot(t,voltage_model)
 hold on
-plot(tt,out)
