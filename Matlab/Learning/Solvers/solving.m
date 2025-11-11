9680d8cf02cf212c5997358abde30e07980d00d3
George Kirby <94544219+Kirbyinmars@users.noreply.github.com>
2025-11-11 16:42:26 +0000

    More mats w temp

--- Matlab/Learning/Solvers/solving.m
+++ Matlab/Learning/Solvers/solving.m
@@ -1,7 +1,7 @@
 cd(fileparts(which('Solving.m')))
 pwd
-load('..\Fixed_Poly\DS_DATA.mat')
-load('..\LiPO_Parameter_Estimation\Data\cycle_1.mat')
+load('../Fixed_Poly/DS_DATA.mat')
+load('../LiPO_Parameter_Estimation/Data/cycle_1.mat')
 
 current_lut = @(t) interp1(tt, u1, t, 'linear', 'extrap');
 % p(12)= 2*p(12); % the problem from before was the soc went negative, and then the ocv went went. it's not used to fitting for negative soc. 
