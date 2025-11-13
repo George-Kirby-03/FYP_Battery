ccc3db21b8ebd7284d6a51be58f0be0dd1008d55
George Kirby <georgekirby2003@gmail.com>
2025-11-13 21:24:03 +0000

    hg

--- Matlab/Learning/Dans_Data_Extraction/cc3.m
+++ Matlab/Learning/Dans_Data_Extraction/cc3.m
@@ -1,4 +1,5 @@
 function [outputArg1,outputArg2] = cc3(param_struct,problem_struct,c1,c2,c3,t_vec)
+% Use tt iclocs timing 
 %CC3 Creates the 3 custom chargin segments, done to reach 80% soc
 stage1_time = 0.2*param_struct.(problem_struct.data.poly.Q)/c1;
 stage2_time = 0.2*param_struct.(problem_struct.data.poly.Q)/c2;
