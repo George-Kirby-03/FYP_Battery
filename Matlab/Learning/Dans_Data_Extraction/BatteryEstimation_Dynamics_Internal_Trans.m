b099f6b789fc203508248ddef52165722c2cab83
George Kirby <georgekirby2003@gmail.com>
2025-12-08 21:02:25 +0000

    njmnhbkhljgbvkhgvhkgv

---
+++ Matlab/Learning/Dans_Data_Extraction/BatteryEstimation_Dynamics_Internal_Trans.m
@@ -0,0 +1,52 @@
+function [dx] = BatteryEstimation_Dynamics_Internal(x,u,p,t,vdat)
+% Double Integrator Dynamics - Internal
+%
+% Syntax:  
+%          [dx] = Dynamics(x,u,p,t,vdat)	(Dynamics Only)
+%          [dx,g_eq] = Dynamics(x,u,p,t,vdat)   (Dynamics and Eqaulity Path Constraints)
+%          [dx,g_neq] = Dynamics(x,u,p,t,vdat)   (Dynamics and Inqaulity Path Constraints)
+%          [dx,g_eq,g_neq] = Dynamics(x,u,p,t,vdat)   (Dynamics, Equality and Ineqaulity Path Constraints)
+% 
+% Inputs:
+%    x  - state vector
+%    u  - input
+%    p  - parameter
+%    t  - time
+%    vdat - structured variable containing the values of additional data used inside
+%          the function%      
+% Output:
+%    dx - time derivative of x
+%    g_eq - constraint function for equality constraints
+%    g_neq - constraint function for inequality constraints
+%
+% Copyright (C) 2019 Yuanbo Nie, Omar Faqir, and Eric Kerrigan. All Rights Reserved.
+% The contribution of Paola Falugi, Eric Kerrigan and Eugene van Wyk for the work on ICLOCS Version 1 (2010) is kindly acknowledged.
+% This code is published under the MIT License.
+% Department of Aeronautics and Department of Electrical and Electronic Engineering,
+% Imperial College London London  England, UK 
+% ICLOCS (Imperial College London Optimal Control) Version 2.5 
+% 1 Aug 2019
+% iclocs@imperial.ac.uk
+
+%
+%------------- BEGIN CODE --------------
+
+SOC = x(:,1); V_RC1 = x(:,2); T = x(:,3);
+
+% Take input measurement directly from Lookup table
+current_bat = vdat.InputCurrent(t);
+
+% Note the battery parameters are no longer saved in vdat, but as static
+% decision variables of the optimisation solution
+%Q=p(:,vdat.poly.Q); 
+Q=1.5*3600;
+C1=p(:,vdat.poly.C); R0=p(:,vdat.poly.R0); R1=p(:,vdat.poly.R1); 
+hA = p(:,vdat.poly.A); mCp = p(:,vdat.poly.CP);
+
+dx(:,1) = current_bat./Q;
+
+dx(:,2) = -V_RC1./(R1.*C1) + current_bat./C1;
+
+dx(:,3) = -(hA./mCp).*(T) + (R0./mCp).*(current_bat).^2 + (1./(mCp)).*V_RC1.*current_bat;
+
+%------------- END OF CODE --------------%
---
+++ Matlab/Learning/Dans_Data_Extraction/main_BatteryEstimation_trans.m
@@ -0,0 +1,79 @@
+% Main script to solve the Battery state and parameter estimation problem
+%
+% Copyright (C) 2019 Yuanbo Nie, Omar Faqir, and Eric Kerrigan. All Rights Reserved.
+% The contribution of Paola Falugi, Eric Kerrigan and Eugene van Wyk for the work on ICLOCS Version 1 (2010) is kindly acknowledged.
+% This code is published under the MIT License.
+% Department of Aeronautics and Department of Electrical and Electronic Engineering,
+% Imperial College London London  England, UK 
+% ICLOCS (Imperial College London Optimal Control) Version 2.5 
+% 1 Aug 2019
+% iclocs@imperial.ac.uk
+
+% voltage=3.64+0.55.*x1-0.72.*x1.^2+0.75*x1.^3+x2+vdat.R0*u1;
+%--------------------------------------------------------
+
+clear all;close all;format compact;
+
+
+
+
+[problem,guess]=BatteryEstimation_temp_trans('B1_CT_trans.mat');          % Fetch the problem definition
+options= problem.settings(350);                  % Get options and solver settings 
+[solution,MRHistory]=solveMyProblem( problem,guess,options);
+
+%% figure%
+
+%% figure
+tt=solution.T;
+x1=speval(solution,'X',1,tt);
+x2=speval(solution,'X',2,tt);
+x3=speval(solution,'X',3,tt);
+u1=problem.data.InputCurrent(tt);
+soc = linspace(0,1,50);
+OCV_SOC = polymodel(problem.data,solution.p,soc,1);
+y=polymodel(problem.data,solution.p,x1,1) + x2 + solution.p(problem.data.poly.R0).*u1;
+temp = x3;
+figure
+subplot(2,3,1)
+hold on
+plot(tt,x1,'b-' ,'LineWidth',2)
+plot([solution.T(1,1); solution.tf],[problem.states.xl(1), problem.states.xl(1)],'r-' )
+plot([solution.T(1,1); solution.tf],[problem.states.xu(1), problem.states.xu(1)],'r-' )
+xlabel('Time [s]')
+ylabel('States: State-of-Charge')
+grid on
+
+subplot(2,3,2)
+hold on
+plot(soc,OCV_SOC,'b-' ,'LineWidth',2)
+xlabel('SOC')
+ylabel('OCV')
+grid on
+
+subplot(2,3,3)
+hold on
+plot(tt,u1,'k-' ,'LineWidth',2)
+plot([solution.T(1,1); solution.tf],[problem.inputs.ul, problem.inputs.ul],'r-' )
+plot([solution.T(1,1); solution.tf],[problem.inputs.uu, problem.inputs.uu],'r-' )
+xlim([0 solution.tf])
+xlabel('Time [s]')
+grid on
+ylabel('Control Input: Current [A]')
+ 
+subplot(2,3,4)
+hold on
+plot(tt,temp,'b-' ,'LineWidth',2)
+plot(tt,problem.data.OutputTemp(tt),'k-' ,'LineWidth',2)
+xlabel('Time [s]')
+ylabel('Output: temperature [DT]')
+legend('Model Output', 'Measured')
+grid on
+
+subplot(2,3,5)
+hold on
+plot(tt,y,'b-' ,'LineWidth',2)
+plot(tt,problem.data.OutputVoltage(tt),'k-' ,'LineWidth',2)
+xlabel('Time [s]')
+ylabel('Output: voltage [V]')
+legend('Model Output', 'Measured')
+grid on
