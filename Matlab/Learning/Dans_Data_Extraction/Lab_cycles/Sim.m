c04e75e975b4f75285deb72cde596a843fc5c828
George Kirby <georgekirby2003@gmail.com>
2025-12-13 14:12:57 +0000

    theend

--- Matlab/Learning/Dans_Data_Extraction/Lab_cycles/Sim.m
+++ Matlab/Learning/Dans_Data_Extraction/Lab_cycles/Sim.m
@@ -1,10 +1,11 @@
 pwd
 clear
+close all
 load('charac_01_s4.mat')
 load('RS_Params.mat')
-p.r1 = 0.07;
+p.r1 = 0.08;
 p.r0 = 0.163 - p.r1;
-p.c = 315;
+p.c = 200;
 p.q = 1.53*60*60;
 p.vu = polyval(ocv_curve,1);
 p.vl = polyval(ocv_curve,0);
@@ -31,3 +32,22 @@ x1=y_sim(:,1);x2=y_sim(:,2);
 voltage_model = polyval(ocv_curve,x1) + x2 + R0.*current_lut(t_sim);
 y_og = interp1(tt, y, t_sim, 'linear', 'extrap');
 plot(t_sim,voltage_model,t_sim,y_og,t_sim,current_lut(t_sim)./10+y(1))
+
+%% Simulate the 3C discharge to comapre with datasheet
+
+current_cc_discharge = -1.5.*[0.1, 0.2, 0.5, 1, 2, 3 ];
+
+for i=1:length(current_cc_discharge)
+u2 = current_cc_discharge(i)*ones(150,1);
+tt = linspace(0,12*60^2,150);
+current_lut = @(t) interp1(tt, u2, t, 'linear', 'extrap');
+[t_sim, y_sim] = ode45(@(t, y) dynamics(t, y, p, current_lut), [0 tt(end)], [1; 0; 0]);
+x1=y_sim(:,1);x2=y_sim(:,2);
+voltage_model = polyval(ocv_curve,x1) + x2 + R0.*current_cc_discharge(i);
+sim_time{i}    = t_sim;
+cap{i}         = -t_sim .*current_cc_discharge(i)*1000/(60^2);
+sim_voltage{i} = voltage_model;
+plot(cap{i}, sim_voltage{i})
+hold on
+grid on
+end
