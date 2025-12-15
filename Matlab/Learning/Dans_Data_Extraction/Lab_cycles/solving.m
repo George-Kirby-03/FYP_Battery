2af951c44165342dfc2ac5dd4764c07b6426c473
George Kirby <georgekirby2003@gmail.com>
2025-12-15 11:26:43 +0000

    prog

--- Matlab/Learning/Dans_Data_Extraction/Lab_cycles/solving.m
+++ Matlab/Learning/Dans_Data_Extraction/Lab_cycles/solving.m
@@ -8,8 +8,10 @@ load('RS_Params.mat')
 % p.r1 = 0.03;
 % p.r0 = 0.035;
 % p.c = 120;
-p.r1 = 0.07;
-p.r0 = 0.163 - p.r1;
+% p.r1 = 0.07;
+% p.r0 = 0.163 - p.r1;
+p.r1 = 0.03;
+p.r0 = 0.075 - p.r1;
 p.c = 315;
 p.q = 1.53*60*60;
 p.vu = polyval(ocv_curve,1);
@@ -63,9 +65,9 @@ colormap default
 heatmap(hrl,Cl,socl)
 
 %% Now, charge CC, upto the point V_ulim is reached, this is when CV takes over
-hr = 3; %%how long charge max
+hr = 4; %%how long charge max
 
-Cl = linspace(0.2,3,15)'; %%CC_rate charge
+Cl = linspace(0.4,4,15)'; %%CC_rate charge
 cc_times = ones(length(Cl),1);
 cv_times = ones(length(Cl),1);
 tt = linspace(0,hr*60*60,150);
@@ -74,41 +76,60 @@ for i=1:length(Cl)
 C = Cl(i);
 current_cc_charge = C*Curr*ones(150,1);
 current_lut = @(t) interp1(tt, current_cc_charge, t, 'linear', 'extrap');
-[t_sim, y] = ode45(@(t, y) dynamics(t, y, p, current_lut), [0 tt(end)], [0; 0; 0]);
+[t_sim, y] = ode15s(@(t, y) dynamics(t, y, p, current_lut), [0 tt(end)], [0; 0; 0]);
 x1=y(:,1);x2=y(:,2); %If Vlim is reached, this soc is not reliable
 vlim_sim_idx = find(y(:,3) > 0, 1, 'first');
-socu_sim_idx = find(y(:,1) >= 0.98, 1, 'first');
+socu_sim_idx = find(y(:,1) >= 0.995, 1, 'first');
 fprintf('##### Current loop (cc rate of %.1f): \n',C)
-if isempty(vlim_sim_idx) && ~isempty(socu_sim_idx) %If its nearly charged just from cc alone, soc will be near 1, and voltage limit not reached
-fprintf(['Time to full-charge (~0.98): %.3fh \n' ...
-    'CV limit not reached i.e no cc needed \n'],t_sim(socu_sim_idx)/60^2);
-    cc_times(i) = 0;
-    cv_times(i) = 0;
-elseif ~isempty(vlim_sim_idx)
+if isempty(vlim_sim_idx)
+     fprintf('Charge time not reached, max SoC: %.2f\n',x1(end,1))
+     cc_times(i) = tt(end);
+     cv_times(i) = 0;
+elseif (~isempty(vlim_sim_idx) && (x1(vlim_sim_idx) < 0.96))  % && (~isempty(socu_sim_idx) && (vlim_sim_idx < socu_sim_idx))) %If vlim is reached before SoC, its clipped and needs CV
 cc_times(i) = t_sim(vlim_sim_idx);
-fprintf('Running CV stage since full SoC not reached before cutoff voltage, (reached %.2f SoC)\n',x1(end))
-[t_sim, y] = ode45(@(t, y) CV_dynamics(t, y, p), [0 tt(end)], [x1(end); x2(end); 0]);
+%voltage_model = polyval(ocv_curve,x1) + x2 + R0.*C*Curr;
+fprintf('Time for CC: %.2f\n',cc_times(i)/60^2)
+fprintf('Running CV stage since full SoC not reached before cutoff voltage, (reached %.3f SoC)\n',x1(vlim_sim_idx))
+[t_sim, y] = ode15s(@(t, y) CV_dynamics(t, y, p), [0 tt(end)], [x1(vlim_sim_idx); x2(vlim_sim_idx); 0]);
 x1=y(:,1);x2=y(:,2);
-hold on
-current = diff(y(:,1).*p.q) ./ diff(t_sim);    
-current = [current;0];
-plot(t_sim,current)
-socu_sim_idx = find(y(:,1) >= 0.999, 1, 'first');
+% current = diff(y(:,1).*p.q) ./ diff(t_sim);    
+% current = [current;0];
+% plot(t_sim,current)
+%fprintf('Last SoC: %.5f, sim time: %.2f \n',x1(end),t_sim(end));
+socu_sim_idx = find(y(:,1) > 0.995, 1, 'first');
 fprintf('Time for CV: %.2fh \n',t_sim(socu_sim_idx)/60^2);
 cv_times(i) = t_sim(socu_sim_idx);
 fprintf('Time to full-charge (~0.99): %.3fh \n', (cv_times(i)+cc_times(i))./60^2)
-else
-    fprintf('Charge time not reached, max SoC: %.2f\n',x1(end,1))
+elseif ~isempty(vlim_sim_idx)
+    cc_times(i) = t_sim(vlim_sim_idx);
+    cv_times(i) = 0;
+    fprintf('Charged fully with CV alone (~0.96SoC) \n')
 end
 fprintf('##### \n')
 end
 
-hold off
+
 
 %%
 
-    plot(Cl,cc_times,Cl,cv_times,Cl,cc_times+cv_times)
-
+    
+grid on
+plot(Cl,cc_times,Cl,cv_times,Cl,cc_times+cv_times)
+legend(["CC Time", "CV Time", "Total Time"], "Position", [0.8148 0.8298 0.0684, 0.0614]);
+title("Charging Times with CC & CV (Lab Properties Used)");
+xlabel("C Current for CC");
+ylabel("Charge Duration (S)");
+hAxes = findobj(gcf,"Type","axes");
+hAxes.LineWidth = 0.5000;
+hAxes.GridLineWidthMode = "auto";
+TotalTime = findobj(gcf,"DisplayName","Total Time");
+TotalTime.LineWidth = 4.5000;
+CCTime = findobj(gcf,"DisplayName","CC Time");
+CCTime.LineWidth = 2;
+CCTime.LineStyle = ":";
+CVTime = findobj(gcf,"DisplayName","CV Time");
+CVTime.LineWidth = 2;
+CVTime.LineStyle = "--";
 
 %%
 
