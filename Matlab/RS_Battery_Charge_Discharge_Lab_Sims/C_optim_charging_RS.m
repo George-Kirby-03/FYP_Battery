b129c61f1fc00c3784bbfd4443f1dbf454c8f59b
George Kirby <georgekirby2003@gmail.com>
2026-02-08 18:49:03 +0000

    C

--- Matlab/RS_Battery_Charge_Discharge_Lab_Sims/C_optim_charging_RS.m
+++ Matlab/RS_Battery_Charge_Discharge_Lab_Sims/C_optim_charging_RS.m
@@ -1,29 +1,61 @@
+close all
+clear
+load RS_Params.mat
+ocv = linspace(0,1,150);
+p.r1 = 0.07;
+p.r0 = 0.16- p.r1;
+
+%To see current limit in C till V cutoff (3.6V)
+% (3.6 - ocv(soc))/R_0+1)/1.5
+
+curve = ((3.6 - polyval(ocv_curve_2,ocv))/(p.r1+p.r0))/1.5;
+figure();
+plot(ocv,curve)
+xlim("auto")
+ylim("auto")
+grid on
+title("\textbf{Permissable Charge Current Along Charge Profile}", "Interpreter", "latex", "FontSize", 17)
+xlabel("SoC", "FontSize", 15, "FontWeight", "bold")
+ylabel("Current (C)", "FontSize", 16, "FontWeight", "bold")
+ 
+ax = gca;
+chart = ax.Children(1);
+datatip(chart,0.8054,1.233);
+datatip(chart,0.604,1.365);
+datatip(chart,0.4027,1.396);
+datatip(chart,0.2013,1.648);
+hLine = findobj(gcf,"Type","line")
+hLine.LineWidth = 4
+
 t = linspace(0, 2, 2000);
 
-CC13_min = 0.6 ./ (t - 0.08);
-CC4_min  = 0.2 ./ (t - 0.279);
+C4 = 0.411
+C13 = 0.163
+
+CC13_min = 0.6 ./ (t - C13);
+CC4_min  = 0.2 ./ (t - C4);
 
 % Mask singularities
-CC13_min(abs(t - 0.08) < 1e-2) = NaN;
-CC4_min(abs(t - 0.279) < 1e-2) = NaN;
+CC13_min(abs(t - C13) < 1e-2) = NaN;
+CC4_min(abs(t - C4) < 1e-2) = NaN;
 
-CC13_min_n  = 0.6 ./ (t - 0.098);
-CC4_min_n  = 0.2 ./ (t - 0.308);
+CC13_min_n  = 0.6 ./ (t - C13);
+CC4_min_n  = 0.2 ./ (t - C4);
 
 % Mask singularities
-CC13_min_n(abs(t - 0.098) < 1e-2) = NaN;
-CC4_min_n(abs(t - 0.308) < 1e-2) = NaN;
+CC13_min_n(abs(t - C13) < 1e-2) = NaN;
+CC4_min_n(abs(t - C4) < 1e-2) = NaN;
 
 figure; hold on; grid on;
 plot(t, CC13_min, 'LineWidth', 1.5)
 plot(t, CC4_min,  'LineWidth', 1.5)
 plot(t, CC13_min_n, 'LineWidth', 1.5)
 plot(t, CC4_min_n,  'LineWidth', 1.5)
-xlim([0.049 1.115])
-ylim([0.20 6.44])
+xlim([0.4 1.115])
+ylim([0.20 4])
 legend(["$\mathbf{CC}_{1:3,min}$", "$\mathbf{CC}_{4,min}$"], "FontSize", 15, "Interpreter", "latex", "LineWidth", 1, "Position", [0.7925 0.8296 0.0906, 0.0673])
 title("Attia to RS Parameter descisions", "FontSize", 18)
-xlabel("$t_{0-80\%} (hours)$", "Position", [0.5562 -0.2034 -1.0000], "Interpreter", "latex", "FontSize", 20)
+xlabel("$t_{0-80\%} (hours)$", "Interpreter", "latex", "FontSize", 20)
 ylabel("$C$", "Interpreter", "latex", "FontSize", 19)
  
 mathbfCC_13minLine = findobj(gcf, "DisplayName", "$\mathbf{CC}_{1:3,min}$")
