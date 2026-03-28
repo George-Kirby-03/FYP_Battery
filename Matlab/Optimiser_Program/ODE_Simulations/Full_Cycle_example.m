f5feea385ec30b061f7a215e00c4aa503f0a3001
George Kirby <georgekirby2003@gmail.com>
2026-03-28 23:44:48 +0000

    nearly

--- Matlab/Optimiser_Program/ODE_Simulations/Full_Cycle_example.m
+++ Matlab/Optimiser_Program/ODE_Simulations/Full_Cycle_example.m
@@ -1,7 +1,7 @@
 % load in the sim_handle
 
 % ODE45 Simulation example 
-
+addpath(genpath('Misc_functions'))
 %Here
 
 %Currently, if CV segments for charge or discharge are diabled, if limits
@@ -19,15 +19,19 @@
 %SoC segment in charging corresponds to 10Q input
 charge_protocol.capacity_selection = 'Discharged'; %or 'Absolute'
 
-charge_protocol.charge_segments = [0,20,40,60,80,100]; %Specify as many segments
-charge_protocol.charge_currents = [2,3,4,5,6]; %Specify current per segment
+charge_protocol.charge_segments = [0,20]; %Specify as many segments
+charge_protocol.charge_currents = [2]; %Specify current per segment
 charge_protocol.CV_cutoff = 0.05; %Segment CV stage (if met) will stop once current falls to this limit
 charge_protocol.discharge_segments = [100 0];
 charge_protocol.discharge_currents = [5];
 charge_protocol.discharge_charge_rest = 60*30; %If set, there will be a rest period between charge and discharge
-charge_protocol.discharge_CV = 'False';
-charge_protocol.charge_CV = 'True';
+charge_protocol.discharge_CV = 'False'; %False will mean once a discharge segment reaches vlim, it will switch to the next.
+%segment instantly without running CV
+charge_protocol.charge_CV = 'True'; %'False' not currently implamented
 charge_protocol.ambient_temp = 24;
 
+warning('off', 'all')
+load baseline1-800.mat
+warning('on', 'all')
 
-sim_results = odeSOC(sim_handler,charge_protocol);
+sim_results = odeSOC(sim_handler{1},charge_protocol);
