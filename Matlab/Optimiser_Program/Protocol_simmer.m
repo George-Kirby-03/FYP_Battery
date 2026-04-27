73ea7cc67aa74b76ad1a2b164eb8b92002d78ecf
George Kirby <georgekirby2003@gmail.com>
2026-04-27 21:11:02 +0100

    sefsfesf

--- Matlab/Optimiser_Program/Protocol_simmer.m
+++ Matlab/Optimiser_Program/Protocol_simmer.m
@@ -7,16 +7,16 @@
 %For Protocol 3 Minimising temp intergral
 
 load MinTempInt-a.mat
-load RS_LiPo_extracted.mat
+load better_ocv.mat
 avg_sim_handle_b1 = sim_average(sim_handler,20,'Start');
-avg_sim_handle_b1 = ocv_fun_injection(avg_sim_handle_b1,ocv_curve_2);
-avg_sim_handle_b1.current_sol.Q = 5.52e03;
+%avg_sim_handle_b1 = ocv_fun_injection(avg_sim_handle_b1,ocv_curve_3);
+%avg_sim_handle_b1.current_sol.Q = 5.5e03;
 avg_sim_handle_b1.current_sol.C = 800;
 avg_sim_handle_b1.current_sol.R0 = 0.075;
 avg_sim_handle_b1.current_sol.R1 = 0.045;
 avg_sim_handle_b1.current_sol.Cp = 100.8;
 avg_sim_handle_b1.current_sol.h = 0.128;
-% load paings1-200b2.mat
+load MinTempInt-a.mat
 % avg_sim_handle_b2 = sim_average(sim_handler,5,'Start');
 
 %avg_sim_handle_b1 = ocv_fun_injection(avg_sim_handle_b1,ocv_curve_2);
@@ -26,8 +26,8 @@ charge_protocol.charge_segments = [0 20 40 60 80 100]; %Specify as many segments
 charge_protocol.charge_currents = [1.3 1.45 1.75 1.53 0.5] * 1.5; %Specify current per segment in c
 charge_protocol.CV_cutoff = 1.5/20; %Segment CV stage (if met) will stop once current falls to this limit
 charge_protocol.discharge_segments = [100 0]; %Specify as many segments as needed
-charge_protocol.discharge_currents = [2.55]*1.5; %Specify current per segment in c here
-charge_protocol.discharge_charge_rest = 63*30; %If set, there will be a rest period between charge and discharge ...
+charge_protocol.discharge_currents = [2.5]*1.5; %Specify current per segment in c here
+charge_protocol.discharge_charge_rest = 60*30; %If set, there will be a rest period between charge and discharge ...
 charge_protocol.discharge_CV = 'False'; %False will mean once a discharge segment reaches vlim, it will switch to the next.
 %segment instantly without running CV, it makes more sense to use this in
 %conjunction with capacity_selection as 'Discharged'
@@ -46,12 +46,12 @@ sim_results = odeSOC(avg_sim_handle_b1,charge_protocol);
 % sim_results.V = V_out;
 %Compare simulated and actual labs
 %Find where the discharge starts
-idx = find(avg_sim_handle_b1.original_data.amps<0,1,'first');
+idx = find(sim_handler{1}.original_data.amps<0,1,'first');
 figure()
 % plot(sim_results.time,sim_results.V);
 hold on
 % %plot(sim_results_b2.time,sim_results_b2.V);
 plot(sim_results.time,sim_results.I,sim_results.time,sim_results.V);
-plot(avg_sim_handle_b1.original_data.ts(idx:end) - avg_sim_handle_b1.original_data.ts(idx),avg_sim_handle_b1.original_data.volts(idx:end));
+plot(sim_handler{1}.original_data.ts(idx:end) - sim_handler{1}.original_data.ts(idx),sim_handler{1}.original_data.volts(idx:end));
 % plot(avg_sim_handle_b1.original_data.ts(idx:end) - avg_sim_handle_b1.original_data.ts(idx),avg_sim_handle_b1.original_data.amps(idx:end))
 
